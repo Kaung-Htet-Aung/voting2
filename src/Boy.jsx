d293384752cce5341708a9711246a3faafe883b7
Kaung Htet Aung <kaunghtetaung374@gmail.com>
2025-01-10 11:21:19 +0630

    time10

--- src/Boy.jsx
+++ src/Boy.jsx
@@ -55,7 +55,7 @@ const Boy = () => {
   const [isVotingOpen, setIsVotingOpen] = useState(false); // Voting status
   const [loading, setLoading] = useState(false);
    const [timeLeft, setTimeLeft] = useState(""); 
-  const votingStartTime = new Date("2025-01-10T12:00:00");
+  const votingStartTime = new Date("2025-01-10T10:00:00");
     const votingEndTime = new Date("2025-01-10T15:00:00");
  // Update visible items based on screen size
   const determineVisibleItems = () => {
